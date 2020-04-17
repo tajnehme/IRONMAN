d1d692d62c3fc93d4f377254542a8107e0bc6d20
tajnehme <tnehme@deloitte.com>
2020-04-17 01:08:03 +0100

    Version number one of the one and only I RON   MAN

---
+++ IRONMAN.go
@@ -0,0 +1,24 @@
+package IRONMAN
+
+import "fmt"
+
+const IRON  = "I AM IRONMAN"
+
+type IRONMAN interface {
+	WHORU()
+
+}
+
+func WHORU()  {
+
+	itb:=`--------------------------------------------------------------------------------------------------
+
+
+
+	`
+
+
+
+	fmt.Println(itb)
+
+}
